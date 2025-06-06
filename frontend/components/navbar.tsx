4b71012382f59ae049e2ec44672f36dd2d9e3e11
james-eo <jamesejike04@gmail.com>
2025-06-06 16:33:11 +0100

    Added ModeToggle

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ModeToggle } from "./mode-toggle";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      Navbar
+      <ModeToggle />
+      {/* Add other navbar items here */}
+    </div>
+  );
 };
 
 export default Navbar;
