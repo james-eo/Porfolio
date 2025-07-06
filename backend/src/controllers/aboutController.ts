cef3738d8288ac432988ce149cde8a776b023ea5
james-eo <jamesejike04@gmail.com>
2025-07-06 15:05:38 +0100

    feat: Added about controller

---
+++ backend/src/controllers/aboutController.ts
@@ -0,0 +1,45 @@
+import type { Request, Response, NextFunction } from "express";
+import About from "../models/about.model";
+import asyncHandler from "../utils/asyncHandler";
+
+// @desc Get about information
+// @route GET /api/about
+// @access Public
+export const getAbout = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const about = await About.findOne();
+    if (!about) {
+      res.status(404);
+      throw new Error("About information not found");
+    }
+    res.status(200).json({
+      success: true,
+      data: about || {},
+    });
+  }
+);
+
+// @desc Create or update about information
+// @route POST /api/about
+// @access Private (Admin only)
+
+export const updateAbout = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let about = await About.findOne();
+    if (about) {
+      // Update existing about information
+      about = await About.findByIdAndUpdate(about._id, req.body, {
+        new: true,
+        runValidations: true,
+      });
+    } else {
+      // Create new about information
+      about = await About.create(req.body);
+    }
+
+    res.status(200).json({
+      success: true,
+      data: about,
+    });
+  }
+);
