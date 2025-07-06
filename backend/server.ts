c3a4f5c59101ae074444b92c39ade8687df6a7a7
james-eo <jamesejike04@gmail.com>
2025-07-06 15:04:09 +0100

    feat: Connected database to the server

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./src/utils/db";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Create an Express application
 
 const app = express();
 const PORT = process.env.PORT || 8000;
