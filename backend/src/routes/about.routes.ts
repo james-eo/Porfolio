50f0a2cc7ebf6f7a5b62cd5c92bc0aa8155b51c0
james-eo <jamesejike04@gmail.com>
2025-07-08 10:57:47 +0100

    feat: Added about routes for the CRUD operations

--- backend/src/routes/about.routes.ts
+++ backend/src/routes/about.routes.ts
@@ -1,8 +1,18 @@
 import express from "express";
-import { getAbout, updateAbout } from "../controllers/aboutController";
+import {
+  getAbout,
+  createAbout,
+  updateAbout,
+  deleteAbout,
+} from "../controllers/aboutController";
 
 const router = express.Router();
 
-router.route("/").get(getAbout).post(updateAbout);
+router
+  .route("/")
+  .get(getAbout)
+  .post(createAbout)
+  .put(updateAbout)
+  .delete(deleteAbout);
 
 export default router;
